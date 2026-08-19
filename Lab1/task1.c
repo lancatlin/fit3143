737ac09a009f398a112515062539348794813ad3
Justin Lin <wancat@wancat.cc>
2026-08-19 19:58:22 +1000

    set indent to 4

--- Lab1/task1.c
+++ Lab1/task1.c
@@ -1,53 +1,29 @@
+#include "runtask.h"
+#include "slice.h"
 #include <math.h>
 #include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 // find all primes up to but not including n
-void find_primes(int n, FILE *f) {
-  if (n < 2)
-    return;
-  int count = 0;
-  for (int i = 2; i < n; i++) {
-    int sqroot = floor(sqrt(i));
-    bool is_prime = true;
-    for (int j = 2; j <= sqroot; j++) {
-      if (i % j == 0) {
-        is_prime = false;
-      }
+IntSlice find_primes(int n) {
+    IntSlice primes = make_slice(0, 10);
+    if (n < 2)
+        return primes;
+    int count = 0;
+    for (int i = 2; i < n; i++) {
+        int sqroot = floor(sqrt(i));
+        bool is_prime = true;
+        for (int j = 2; j <= sqroot; j++) {
+            if (i % j == 0) {
+                is_prime = false;
+            }
+        }
+        if (is_prime) {
+            append_slice(&primes, i);
+        }
     }
-    if (is_prime) {
-      fprintf(f, "%d\n", i);
-      count += 1;
-    }
-  }
-  fprintf(f, "Total: %d\n", count);
+    return primes;
 }
 
 int main(int argc, char *argv[]) {
-  int n = 100;
-  if (argc >= 2) {
-    n = atoi(argv[1]);
-  }
-  printf("N: %d\n", n);
-  FILE *f = stdout;
-  if (n > 100) {
-    if (argc >= 3) {
-      f = fopen(argv[2], "w");
-    } else {
-      f = fopen("output.txt", "w");
-    }
-    if (f == NULL) {
-      printf("Cannot open file\n");
-      return 1;
-    }
-  }
-
-  clock_t start = clock();
-  find_primes(n, f);
-  clock_t end = clock();
-  fprintf(f, "Duration: %.4fs\n", (double)(end - start) / CLOCKS_PER_SEC);
-  fclose(f);
-  return 0;
+    return run_task(argc, argv, find_primes, "task1");
 }
--- Lab1/task3.c
+++ Lab1/task3.c
@@ -3,36 +3,35 @@
 #include <math.h>
 #include <omp.h>
 #include <stdbool.h>
-#include <stdio.h>
 
 // find all primes up to but not including n
 IntSlice find_primes(int n) {
-  IntSlice primes = make_slice(0, 10);
-  if (n < 2)
-    return primes;
+    IntSlice primes = make_slice(0, 10);
+    if (n < 2)
+        return primes;
 
 #pragma omp parallel for schedule(dynamic, 10) ordered
-  for (int i = 2; i < n; i++) {
+    for (int i = 2; i < n; i++) {
 
-    int sqroot = floor(sqrt(i));
-    bool is_prime = true;
-    for (int j = 2; j <= sqroot; j++) {
-      if (i % j == 0) {
-        is_prime = false;
-        break;
-      }
-    }
-    if (is_prime) {
-      // printf("Thread %d: i: %d\n", omp_get_thread_num(), i);
+        int sqroot = floor(sqrt(i));
+        bool is_prime = true;
+        for (int j = 2; j <= sqroot; j++) {
+            if (i % j == 0) {
+                is_prime = false;
+                break;
+            }
+        }
+        if (is_prime) {
+            // printf("Thread %d: i: %d\n", omp_get_thread_num(), i);
 #pragma omp ordered
-      {
-        append_slice(&primes, i);
-      }
+            {
+                append_slice(&primes, i);
+            }
+        }
     }
-  }
-  return primes;
+    return primes;
 }
 
 int main(int argc, char *argv[]) {
-  return run_task(argc, argv, find_primes, "task3");
+    return run_task(argc, argv, find_primes, "task3");
 }
