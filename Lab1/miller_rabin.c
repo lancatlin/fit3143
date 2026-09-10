2960d39e2392e59bfa3035fc6ee58c44f41a010f
Justin Lin <wancat@wancat.cc>
2026-09-10 16:30:09 +1000

    feat: separate repeated_squaring and miller rabin

--- Lab1/miller_rabin.c
+++ Lab1/miller_rabin.c
@@ -14,35 +14,45 @@
 // Return a random number from 2 to n-2
 long rand_base(int n) { return rand() % (n - 3) + 2; }
 
+long repeated_squaring(int a, int b, int n) {
+    // return a^b mod n
+    // printf("repeated squaring: a %d, b %d, n %d\n", a, b, n);
+    long x = 1;
+    long d = floor(log2(b)) + 1;
+    for (int i = d - 1; i >= 0; i--) {
+        bool bit = b >> i & 1;
+        x = (x * x) % n;
+        if (bit) {
+            x = (x * a) % n;
+        }
+    }
+    return x;
+}
+
 bool miller_rabin(long a, long n) {
     // return a^b mod n
-    // printf("repeated squaring: a %ld, n %ld\n", a, n);
-    long x = 1;
-    long b = n - 1;
-    long t = b;
+    long t = n - 1;
     long s = 0;
     while (t % 2 == 0) {
         s++;
         t >>= 1;
     }
 
-    long d = floor(log2(b)) + 1;
+    long x = repeated_squaring(a, t, n);
 
-    for (long i = 0; i < d; i++) {
-        bool bit = b >> (d - i - 1) & 1;
+    if (x == 1 || x == n - 1) {
+        return true;
+    }
+
+    for (long i = 0; i < s; i++) {
         x = (x * x) % n;
-        if (bit) {
-            x = (x * a) % n;
-        }
-        if (i >= (d - s)) {
-            if (x == n - 1) {
-                return i != d;
-            } else if (x == 1) {
-                return i == d - s;
-            }
+        if (x == n - 1) {
+            return i != s - 1;
+        } else if (x == 1) {
+            return false;
         }
     }
-    return x == 1;
+    return false;
 }
 
 bool is_prime(int n, int k) {
