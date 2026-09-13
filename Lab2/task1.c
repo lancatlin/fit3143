fb37b53e4126b52cd44c560465d5ebe6a05f6da9
Justin Lin <wancat@wancat.cc>
2026-09-13 16:05:48 +1000

    feat: send custom struct

--- Lab2/task1.c
+++ Lab2/task1.c
@@ -11,21 +11,21 @@
 
 int mpi_rank = 0;
 int mpi_size = 1;
+MPI_Datatype Request;
 
 struct ProcessRequest {
-    int startNumber; // Range includes start number,
-    int endNumber;   // But not end number
+    int start; // Range includes start number,
+    int end;   // But not end number
 };
 
-// find all primes up to but not including n
-int find_primes(int n, IntSlice is_primes) {
+int find_primes(struct ProcessRequest req, IntSlice is_primes) {
     // input validation
-    if (n < 2)
+    if (req.end < 2)
         return 0;
 
     // prime finding algorithm
     int count = 0;
-    for (int i = 2; i < n; i++) {
+    for (int i = req.start; i < req.end; i++) {
         int sqroot = floor(sqrt(i));
         bool is_prime = true;
         for (int j = 2; j <= sqroot; j++) {
@@ -43,15 +43,47 @@ int find_primes(int n, IntSlice is_primes) {
     return count;
 }
 
+// find all primes up to but not including n
+int dispatch_jobs(int n, IntSlice is_primes) {
+    struct ProcessRequest req = {2, n};
+    MPI_Bcast(&req, 1, Request, 0, MPI_COMM_WORLD);
+
+    printf("Received: start: %d, end: %d\n", req.start, req.end);
+    return 0;
+}
+
 int main(int argc, char *argv[]) {
+    struct ProcessRequest req;
+
+    MPI_Datatype type[2] = {MPI_INT, MPI_INT};
+    int blocklen[2] = {1, 1};
+    MPI_Aint disp[2];
+
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
 
+    MPI_Get_address(&req.start, &disp[0]);
+    MPI_Get_address(&req.end, &disp[1]);
+
+    // Make relative
+    disp[1] = disp[1] - disp[0];
+    disp[0] = 0;
+
+    MPI_Type_create_struct(2, blocklen, disp, type, &Request);
+    MPI_Type_commit(&Request);
+
     printf("Rank: %d, size: %d\n", mpi_rank, mpi_size);
 
-    int exit_code = run_task(argc, argv, find_primes, "task1");
+    if (mpi_rank == 0) {
+        int exit_code = run_task(argc, argv, dispatch_jobs, "task1");
+    } else {
+        struct ProcessRequest req;
+        MPI_Bcast(&req, 1, Request, 0, MPI_COMM_WORLD);
+        printf("Received. Rank: %d, start: %d, end: %d\n", mpi_rank, req.start,
+               req.end);
+    }
 
     MPI_Finalize();
-    return exit_code;
+    return 0;
 }
